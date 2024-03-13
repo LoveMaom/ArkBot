9e3d3fc30d5e35d7c6a84358a9ef83f5645e1178
Love_Maom <1700721266@qq.com>
2024-03-13 11:41:59 +0800

    无立绘 自动下载

--- src/main/kotlin/plugins/ImageInfo.kt
+++ src/main/kotlin/plugins/ImageInfo.kt
@@ -4,11 +4,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import java.awt.image.BufferedImage
 import java.io.File
-import java.net.URL
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.concurrent.CopyOnWriteArrayList
 import javax.imageio.ImageIO
+
 
 object ImageInfo {
     // https://prts.wiki/images/c/c0/立绘_干员名字_1.png?image_process=format,webp/quality,Q_90  = 立绘png
@@ -16,41 +22,51 @@ object ImageInfo {
     // https://static.prts.wiki/charinfo/img/star_6.png = 星级图片
     suspend fun image(event: GroupMessageEvent, memberAtt: CopyOnWriteArrayList<String>?, getMember: CopyOnWriteArrayList<String>?, name: String) {
         //背景图片
-        val bgDefault = ImageIO.read(getUrlImage("https://prts.wiki/images/5/59/Bg_default.png","背景"))
-        //干员立绘图片
-        val wikiImage = ImageIO.read(getUrlImage("https://prts.wiki/images/c/c0/立绘_${name}_1.png?image_process=format,webp/quality,Q_90","立绘"))
-        //干员星级图片
-        val star = ImageIO.read(getUrlImage("https://static.prts.wiki/charinfo/img/star_${getMember!![4]}.png","星"))
+        val bgDefault = withContext(Dispatchers.IO) {
+            ImageIO.read(File("data/com.white.ArkBot/default/Bg_default.png"))
+        }
 
+        //立绘图片
+        val imageRead = File("data/com.white.ArkBot/干员默认立绘/${name}.png")
+        //若本地没有立绘则开始下载
+        if (!imageRead.isFile) {
+            //获取的网站 url
+            val imageUrl = "https://prts.wiki/w/文件:立绘_${name}_1.png"
+            //将获取的网站 url 中的图片链接放进列表
+            val getUrl: Elements = Jsoup.connect(imageUrl).get().select("meta[property=og:image]")
+            //发起 Http 链接，从获取到的图片链接提取图片
+            val doc = Jsoup.connect(getUrl[0].attr("content")).ignoreContentType(true).execute().bodyAsBytes()
+            //保存图片的路径和格式
+            val outputFile = File("data/com.white.ArkBot/干员默认立绘/${name}.png")
+            try {
+                //写入目标路径
+                withContext(Dispatchers.IO) {
+                    FileOutputStream(outputFile).use { fos ->
+                        fos.write(doc)
+                    }
+                }
+                println("该干员立绘不存在 成功下载到本地：${outputFile.absolutePath}")
+            } catch (e: IOException) {
+                println("无法下载图片：$e")
+            }
+        }
         //设置图片大小并创建
         val height = 1080
         val width = 1920
-        val getImage = BufferedImage(height,width,BufferedImage.TYPE_INT_RGB)
+        val getImage = BufferedImage(width,height,BufferedImage.TYPE_INT_RGB)
         val graphics = getImage.createGraphics()
 
         //设置背景图片
         graphics.drawImage(bgDefault,0,0,1920,1080,null)
 
         //创建临时文件
-        val tempFile = File.createTempFile(name, ".png")
-        ImageIO.write(getImage,"png",tempFile)
-
-        event.group.sendMessage(tempFile.uploadAsImage(event.group))
-    }
-    // 下载头像图片
-    private suspend fun getUrlImage(url: String,name: String): File {
-        // 获取图片
-        val urlFile = File("${name}.png")
-
-        // 下载图片
+        val tempFile = withContext(Dispatchers.IO) {
+            File.createTempFile(name, ".png")
+        }
         withContext(Dispatchers.IO) {
-            URL(url).openStream()
-        }.use { input ->
-            urlFile.outputStream().use { output ->
-                input.copyTo(output)
-            }
+            ImageIO.write(getImage, "png", tempFile)
         }
 
-        return urlFile
+        event.group.sendMessage(tempFile.uploadAsImage(event.group))
     }
 }
--- src/main/kotlin/plugins/Wiki.kt
+++ src/main/kotlin/plugins/Wiki.kt
@@ -12,7 +12,6 @@ object Wiki {
     suspend fun command(event: GroupMessageEvent) {
         if ("查询干员" == event.message.content.take(4)) {
             val operators = event.message.content.split("查询干员")[1]
-            println("值:" + Common.star!!.select("div[data-zh=${operators}]").text())
             try {
                 //从干员列表中查询是否有该干员
                 if (Common.star!!.select("div[data-zh=${operators}]").text() != "") {
@@ -22,7 +21,6 @@ object Wiki {
                     val list: Elements = doc.select("div[class=mw-parser-output]")
                     //调用图片制作
                     ImageInfo.image(event, Att.main(list), GetMember.main(operators, list), operators)
-                    println("执行成功")
                 } else { event.group.sendMessage("没有这个干员") }
             } catch (_: IOException) {
             } catch (e: Exception) {
